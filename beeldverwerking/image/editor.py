25b4660f643af8bd2d8a5a1960f3026306b595e5
Nic Jenneboer <nicjenneboer@gmail.com>
2022-03-28 15:00:48 +0200

    Added image converter from bitmap to vector (svg)

--- beeldverwerking/image/editor.py
+++ beeldverwerking/image/editor.py
@@ -3,7 +3,9 @@ import io
 import cv2
 import numpy as np
 from base64 import b64encode
-  
+import os
+from ament_index_python.packages import get_package_share_directory
+
 ## IMAGE EDITOR ##
 
 def encode(image):
@@ -35,6 +37,14 @@ def editImage(image):
     contours, heirarchy = cv2.findContours(countoursImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     cv2.drawContours(countoursImage, contours, -1, (0,255,0), 2)
+
+    # ROS2 Package path
+    rp = get_package_share_directory('beeldverwerking')
+    
+
+    cv2.imwrite(rp + '/tmp.bmp', countoursImage)
+
+    os.system("potrace " + rp + "/tmp.bmp --svg -o" + rp + "/tmp.svg")
 
     #Return Images
     return [{
