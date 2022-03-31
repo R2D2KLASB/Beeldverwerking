fa064d606473216c1bba6fd74431f6120e8eb425
Nick Teeuwen <43675529+n21twn@users.noreply.github.com>
2022-03-31 14:07:24 +0200

    Camera module

---
+++ camera_module.py
@@ -0,0 +1,17 @@
+from time import sleep
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+camera.start_preview()
+
+
+while True:
+        if GPIO.input(37)== GPIO.HIGH:
+                print("Smile")
+                camera.capture('assets/Picture.jpg')
+                sleep(2)
