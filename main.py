14358e24618869dcf8bf7cfe20c465684e49a21b
Nic Jenneboer <nicjenneboer@gmail.com>
2022-05-31 10:51:58 +0200

    update

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ class GameEngine():
         self.row_size = 10
         self.col_size = 10
         print('Player 1 Setup Ships')
-        self.player1 = Player()
-        print('Player 1 Setup Ships')
-        self.player2 = Player()
+        self.player1 = Player(self.row_size, self.col_size)
+        print(self.player1)
+        print('Player 2 Setup Ships')
+        self.player2 = Player(self.row_size, self.col_size)
+        print(self.player2)
 
 
 
 class Player():
-    def __init__(self):
-        self.shipBoard = shipBoard(10,10)
-        self.targetBoard = targetBoard(10,10)
+    def __init__(self, row_size, col_size):
+        self.shipBoard = shipBoard(row_size,col_size)
+        self.targetBoard = targetBoard(row_size,col_size)
+        self.row_size = row_size
+        self.col_size = col_size
         self.setupBoard()
 
     def setupBoard(self):
-        shipSizes = [2,3,4]
+        shipSizes = [2]
         print(self.shipBoard)
         while len(shipSizes) > 0:
             print('Available Ships: ' + str(shipSizes))
@@ -38,7 +42,23 @@ class Player():
             else:
                 print('Error ship orientation')
     
-    # def Attack(self,player, coordinate):
+    def Attack(self, player, targetplayer, coordinate):
+        if coordinate in player.targetBoard.coordinates:
+            return False
+        else:
+            player.targetBoard.coordinates += coordinate
+
+
+
+    def __str__(self):
+        string = 'Shipboard:                      Targetboard:\n'
+        for row in range(self.row_size):
+            string += self.shipBoard.__repr__()[row] + "\t\t" + self.targetBoard.__repr__()[row] + "\n"
+        return string
+
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Board():
@@ -50,12 +70,14 @@ class Board():
     def __str__(self):
         string = "\n   " + " ".join(chr(x + ord('A')) for x in range(0, self.col_size )) + "\n"
         for r in range(self.row_size):
-            string += str(r + 1) + ("  " if r <= 8 else " ") +  " ".join(str('-' if self.checkCordinaatAnyShip(Coordinate([c+1,r+1])) else self.board[c][r]) for c in range(self.col_size))
+            string += str(r + 1) + ("  " if r <= 8 else " ") +  " ".join(str(self.board[c][r]) for c in range(self.col_size)) + "\n"
         return string
 
     def __repr__(self):
-        return self.__str__()
-
+        rows = ["   " + " ".join(chr(x + ord('A')) for x in range(0, self.col_size ))]
+        for r in range(self.row_size):
+            rows += [str(r + 1) + ("  " if r <= 8 else " ") +  " ".join(str(self.board[c][r]) for c in range(self.col_size))]
+        return rows
 
 class shipBoard(Board):
     def __init__(self, row_size, col_size):
@@ -89,15 +111,16 @@ class shipBoard(Board):
             string += str(r + 1) + ("  " if r <= 8 else " ") +  " ".join(str('-' if self.checkCordinaatAnyShip(Coordinate([c+1,r+1])) else self.board[c][r]) for c in range(self.col_size)) + "\n"
         return string
 
-    def __str__(self):
-        string = "\n   " + " ".join(chr(x + ord('A')) for x in range(0, self.col_size )) + "\n"
+    def __repr__(self):
+        rows = ["   " + " ".join(chr(x + ord('A')) for x in range(0, self.col_size ))]
         for r in range(self.row_size):
-            string += str(r + 1) + ("  " if r <= 8 else " ") +  " ".join(str('-' if self.checkCordinaatAnyShip(Coordinate([c+1,r+1])) else self.board[c][r]) for c in range(self.col_size)) + "\n"
-        return string
+            rows += [str(r + 1) + ("  " if r <= 8 else " ") +  " ".join(str('-' if self.checkCordinaatAnyShip(Coordinate([c+1,r+1])) else self.board[c][r]) for c in range(self.col_size))]
+        return rows
 
 class Ship():
     def __init__(self, coordinates):
         self.coordinates = coordinates
+        self.hitcoordinates = []
         self.size = len(coordinates)
 
     def checkCoordinate(self, coordinate):
@@ -108,7 +131,7 @@ class Ship():
 class targetBoard(Board):
     def __init__(self, row_size, col_size):
         super().__init__(row_size, col_size)
-        self.targetCoordinates = []
+        self.coordinates = []
 
 
 
